6c6ce4ad753e268faf7d22850c0cf67ac6d14081
DhirenTheHeadlights <dhirenj03@gmail.com>
2024-03-17 22:02:53 -0500

    The most important changes in the code are related to the calculation of the angle between the enemy ship and the player ship, the normalization of this angle, and the determination of the firing side of the cannon.
    1. The calculation of the angle between the enemy ship and the player ship has been adjusted to use a more accurate value of pi. This change will improve the precision of the angle calculation.
    
    2. The normalization of the angle to the range [0, 360] has been removed. This change will allow the angle to be in the range [-360, 360], which will be useful for the next changes.
    
    3. The enemy ship's current rotation is now retrieved and adjusted to the same 0-360 degree range. This change will ensure that the enemy ship's rotation is in the same range as the calculated angle.
    
    4. The calculated angle is now adjusted by subtracting the enemy ship's rotation. This change will make the angle relative to the enemy ship's rotation.
    
    5. The adjusted angle is normalized to the range [-360, 360] using the fmod function. This change will ensure that the adjusted angle is in the correct range.
    
    6. If the adjusted angle is negative, 360 is added to bring it into the range [0, 360]. This change will ensure that the adjusted angle is always positive.
    
    7. The determination of the firing side of the cannon has been changed to use the adjusted angle instead of the original angle. This change will make the firing side determination more accurate. If the adjusted angle is between 90 and 270 degrees, the firing side is set to Port. Otherwise, it is set to Starboard. This assumes 0 degrees is directly to the right, and degrees increase clockwise.

--- PirateGame/Resource/EnemyShipInputHandler_PG.cpp
+++ PirateGame/Resource/EnemyShipInputHandler_PG.cpp
@@ -15,7 +15,6 @@ void EnemyShipInputHandler::handleCannonAim() {
     // Set the manual aim flag to true
     SCH->setFreeAim(true);
 
-    // Set the firing side of the ship based on the player's position
     // Calculate vector from enemy ship to player ship
     sf::Vector2f directionVector = playerPos - sprite.getPosition();
 
@@ -23,15 +22,23 @@ void EnemyShipInputHandler::handleCannonAim() {
     float angleToHorizontal = atan2(directionVector.y, directionVector.x);
 
     // Convert radians to degrees
-    float angleInDegrees = angleToHorizontal * (180.f / 3.141526f);
-    
-    // Normalize the angle to the range [0, 360]
-    while (angleInDegrees < 0) angleInDegrees += 360.f;
-    while (angleInDegrees >= 360.f) angleInDegrees -= 360.f;
+    float angleInDegrees = angleToHorizontal * (180.f / 3.14159f);
+
+    // Get the enemy ship's current rotation and adjust it to the same 0-360 degree range
+    float shipRotation = fmod(sprite.getRotation(), 360.f);
+    if (shipRotation < 0) shipRotation += 360.f; // Not necessary, but just in case
+
+    // Adjust the calculated angle by subtracting the enemy ship's rotation
+    float adjustedAngle = angleInDegrees - shipRotation;
+
+    // Use fmod to normalize the adjusted angle to the range [-360, 360)
+    adjustedAngle = fmod(adjustedAngle, 360.f);
+
+    // If the result is negative, add 360 to bring it into the range [0, 360)
+    if (adjustedAngle < 0) adjustedAngle += 360.f;
 
     // Determine side based on the adjusted angle
-    // This assumes 0 degrees is directly to the right, and degrees increase clockwise.
-    if (angleInDegrees > 90.f && angleInDegrees < 270.f) getCannonHandler()->setFiringSide(FiringSide::Port);
+    if (adjustedAngle > 90.f && adjustedAngle < 270.f) getCannonHandler()->setFiringSide(FiringSide::Port);
     else getCannonHandler()->setFiringSide(FiringSide::Starboard);
 }
 
