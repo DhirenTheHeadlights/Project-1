72e4cc7e581b2c29b5b98a072c0d34c8b5229455
Scott Bellows <scbellows@outlook.com>
2024-08-22 18:03:50 -0400

    Vsync turned off in Globals.cpp

--- Globals/Resource/Globals.cpp
+++ Globals/Resource/Globals.cpp
@@ -6,6 +6,6 @@ sf::RenderWindow window;
 void initializeGlobals(std::string name) {
     desktop = sf::VideoMode::getDesktopMode();           // Initialize the global desktop variable
     window.create(desktop, name, sf::Style::Fullscreen);    // Initialize the global window variable
-    window.setVerticalSyncEnabled(true);
+    window.setVerticalSyncEnabled(false);
     std::srand(static_cast<unsigned int>(std::time(0))); // Initialize the random number generator
 }
